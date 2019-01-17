09b5f5534b24fbbed4be66df24839fbbcd047f8e
phoenix <vanguelisk@gmail.com>
2019-01-17 12:48:06 +0200

    remove spect

--- src/book/people/Spectator.js
+++ src/book/people/Spectator.js
@@ -1,101 +1,106 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './spectator.css';
 
 class Spectator extends Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.pickSeat = this.pickSeat.bind(this);
-        this.removeSpect = this.removeSpect.bind(this);
+    this.pickSeat = this.pickSeat.bind(this);
+    this.removeSpect = this.removeSpect.bind(this);
+  }
 
-   }
-
-    removeSpect(){
-        console.log('removing ' + this.props.specData.id);
-        this.props.removeSpect(this.props.specData.id);
-
-    }
+  removeSpect() {
+    console.log(`removing ${this.props.specData.id}`);
+    this.props.removeSpect(this.props.specData.id);
+  }
 
 
-   pickSeat()
-   {
+  pickSeat() {
     this.props.pickSeat(this.props.specData.id);
-   }
-    render() {
+  }
+
+  render() {
+    const colorClass = (this.props.specData.selectedForSeat) ? ' bg-success ' : ' bg-info ';
+
+    return (
+      <section className="spectatorElem">
 
 
-        const colorClass = (this.props.specData.selectedForSeat) ? ' bg-success ' : ' bg-info ';
+        <div className="card">
+          <div className={`card-header ${colorClass}`}>
+            <div className="row">
+              <div className="col-5">
 
-        return (
-            <section className="spectatorElem">
+                                Spectator #
+                {this.props.specData.humanId}
 
+              </div>
 
-                <div className="card">
-                    <div className={`card-header ${colorClass}`}>
-                        <div className="row">
-                            <div className="col-5">
-                                Spectator # {this.props.specData.humanId}
+              <div className="col-2"><i className="fas fa-eye" /></div>
 
-                            </div>
+              <div className="col-4">
+                <button className="btn btn-primary btn-warning" onClick={this.pickSeat}>Pick Seat</button>
+              </div>
 
-                            <div className="col-2"><i className="fas fa-eye"/></div>
-
-                            <div className="col-4">
-                                <button className="btn btn-primary btn-warning" onClick={this.pickSeat}>Pick Seat</button>
-                            </div>
-
-                        </div>
+            </div>
 
 
-                    </div>
-                    <div className="card-body">
+          </div>
+          <div className="card-body">
 
-                        <div className="row">
-                            <div className="col-4">
-                                Seat {this.props.specData.seat}
-                            </div>
+            <div className="row">
+              <div className="col-4">
 
-                            <div className="col-1">
-                                <label htmlFor="exampleFormControlSelect1">Type </label>
+                                Seat
+                {this.props.specData.seat}
+              </div>
 
-                            </div>
-                            <div className="col-3">
-                                <div className="form-group">
-                                    <select className="form-control" id="exampleFormControlSelect1">
-                                        <option value="ADT">Adult</option>
-                                        <option value="CHD">Child</option>
-                                        <option  value="ELD">Elder</option>
-                                        <option value="STD">Student
-                                        </option>
-                                    </select>
-                                </div>
-                            </div>
+              <div className="col-1">
+                <label htmlFor="exampleFormControlSelect1">Type </label>
 
-                            <div className="col-4">
-                                Seat Cost {this.props.specData.price} &euro;
-                            </div>
-
-
-                        </div>
-                    </div>
-
-                    <div className="card-footer">
-                        <div className="row">
-                            <div className="col-5 offset-4">
-                                <button  className="btn btn-danger" onClick={this.removeSpect}>
-                                   Remove Spectator</button>
-                            </div>
-                        </div>
-                    </div>
-
+              </div>
+              <div className="col-3">
+                <div className="form-group">
+                  <select className="form-control" id="exampleFormControlSelect1">
+                    <option value="ADT">Adult</option>
+                    <option value="CHD">Child</option>
+                    <option value="ELD">Elder</option>
+                    <option value="STD">
+Student
+                    </option>
+                  </select>
                 </div>
+              </div>
 
-            </section>
+              <div className="col-4">
 
-        );
-    }
+                                Seat Cost
+                {this.props.specData.price}
+                {' '}
+&euro;
+              </div>
+
+
+            </div>
+          </div>
+
+          <div className="card-footer">
+            <div className="row">
+              <div className="col-5 offset-4">
+                <button className="btn btn-danger" onClick={this.removeSpect}>
+
+                                   Remove Spectator
+                </button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </section>
+
+    );
+  }
 }
 
 export default Spectator;
-
-
